3cbcd4b3a568b9d9fcf00ec9463ba4ba8a57df64
song_zhiobo <song_zhibo@dahuatech.com>
2020-10-16 15:30:00 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ use App\Http\Controllers\StaticPagesController;
 Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
 Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
+
+Route::get('/signup', [UserController::class, 'create'])->name('signup');
