3045d1d1bde41e995c4d78da11531b08f612cc50
song_zhiobo <song_zhibo@dahuatech.com>
2020-10-19 17:25:00 +0800

    查看用户列表

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,18 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
            'only' => ['create']
         ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(5);
+        return view('users.index', compact('users'));
     }
 
     public function create()
